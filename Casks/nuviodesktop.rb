2009b91fee2d92d8ffb264ca8424eda29cfa3e02
Kadir Metin <82063998+kadirmetin@users.noreply.github.com>
2026-06-26 22:43:07 +0300

    Modify NuvioDesktop cask for latest version
    Updated NuvioDesktop cask to use latest version and no check for SHA256.

--- Casks/nuviodesktop.rb
+++ Casks/nuviodesktop.rb
@@ -1,14 +1,13 @@
 cask "nuviodesktop" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "0.1.9-alpha"
-  sha256 arm:   "6cbdc734454c2171a02433c82b1b121ddaf6bd62b537d56e94b932d47c1b8dae",
-         intel: "50bc1a42e0e7c851eedc96de92e934582528e1290ea25f87f18c6b12de6dc0f5"
+  version :latest
+  sha256 :no_check
 
-  url "https://github.com/NuvioMedia/NuvioDesktop/releases/download/#{version}/Nuvio-macOS-#{arch}-#{version}.dmg"
+  url "https://github.com/NuvioMedia/NuvioDesktop/releases/latest/download/Nuvio-macOS-#{arch}.dmg"
   name "NuvioDesktop"
   desc "Nuvio Desktop Application"
   homepage "https://github.com/NuvioMedia/NuvioDesktop"
 
-  app "NuvioDesktop.app"
+  app "Nuvio.app"
 end
