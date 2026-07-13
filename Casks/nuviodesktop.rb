ad8ec4f6c8040b674a14622a06751ecb430597d7
Kadir Metin <82063998+kadirmetin@users.noreply.github.com>
2026-07-13 18:14:10 +0300

    Update Nuvio Desktop version to 0.1.13-alpha

--- Casks/nuviodesktop.rb
+++ Casks/nuviodesktop.rb
@@ -1,7 +1,7 @@
 cask "nuviodesktop" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "0.1.11-alpha"
+  version "0.1.13-alpha"
   sha256 :no_check
 
   url "https://github.com/NuvioMedia/NuvioDesktop/releases/download/#{version}/Nuvio-macOS-#{arch}-#{version}.dmg"
