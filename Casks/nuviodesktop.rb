4b12c5e89f4001a5c7f08bd9d1187da3c8142ecc
Kadir Metin <82063998+kadirmetin@users.noreply.github.com>
2026-08-19 09:10:46 +0300

    Bump nuviodesktop version to 0.1.20-alpha

--- Casks/nuviodesktop.rb
+++ Casks/nuviodesktop.rb
@@ -1,7 +1,7 @@
 cask "nuviodesktop" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "0.1.19-alpha"
+  version "0.1.20-alpha"
   sha256 :no_check
 
   url "https://github.com/NuvioMedia/NuvioDesktop/releases/download/#{version}/Nuvio-macOS-#{arch}-#{version}.dmg"
